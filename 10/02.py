3259f7c3e9de79e04b20cffa9e2ad5344623f293
Andrew Yale <a.yale9@gmail.com>
2015-12-21 16:50:16 -0500

    day 10

---
+++ 10/02.py
@@ -0,0 +1,20 @@
+current = '3113322113'
+
+
+for _ in range(50):
+    next = ''
+    loc = 0
+    while loc < len(current):
+        char = current[loc]
+        count = 0
+        while loc < len(current) and current[loc] == char:
+            count += 1
+            loc += 1
+
+        next += str(count) + char
+
+
+    current = next
+
+
+print len(current)
