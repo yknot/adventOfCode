7c9e34b43c7df6c403c802285f016527d2a17a3a
Andrew Yale <a.yale9@gmail.com>
2015-12-23 18:37:39 -0500

    day 12

---
+++ 12/02.py
@@ -0,0 +1,29 @@
+import json
+
+data = json.loads(open('input').read())
+
+
+def goDeeper(data):
+    total = 0
+    if type(data) == list:
+        for d in data:
+            total += goDeeper(d)
+    elif type(data) == dict:
+        if 'red' not in data.values():
+            for d in data.keys():
+                total += goDeeper(data[d])
+    elif type(data) == int:
+        return data
+    elif type(data) == unicode:
+        return 0
+    else:
+        print type(data)
+
+    return total
+
+
+total = goDeeper(data)
+
+print total
+
+
