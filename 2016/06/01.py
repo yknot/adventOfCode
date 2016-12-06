614184310371a53472820dd90a11dfe9011e5622
Andrew Yale <a.yale9@gmail.com>
2016-12-06 12:22:03 -0500

    day 6

---
+++ 2016/06/01.py
@@ -0,0 +1,32 @@
+"""advent of code 2016 day 6 part 1"""
+data = open('input').read().splitlines()
+# data = ['eedadn',
+#         'drvtee',
+#         'eandsr',
+#         'raavrd',
+#         'atevrs',
+#         'tsrnev',
+#         'sdttsa',
+#         'rasrtv',
+#         'nssdts',
+#         'ntnada',
+#         'svetve',
+#         'tesnvt',
+#         'vntsnd',
+#         'vrdear',
+#         'dvrsen',
+#         'enarar']
+
+length = len(data[0])
+final = ''
+for i in range(length):
+    counts = {}
+    for d in data:
+        if d[i] in counts:
+            counts[d[i]] += 1
+        else:
+            counts[d[i]] = 1
+
+    final += {v: k for k, v in counts.items()}[max(counts.values())]
+
+print(final)
