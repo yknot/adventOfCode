6104ad731f1dd0d5bec699d3e54bf4b3dda7859a
Andrew Yale <a.yale9@gmail.com>
2016-12-19 11:29:18 -0500

    day 10

---
+++ 2016/10/01.py
@@ -0,0 +1,122 @@
+"""advent of code 2016 day 10 part 1"""
+
+class Bot(object):
+    """class to store bot data"""
+    def __init__(self, b_id):
+        self.id = b_id
+        self.low = -1
+        self.high = -1
+        self.rules = {}
+
+    def add_chip(self, val):
+        """set chip value based on value passed in"""
+        if self.low != -1 and self.high != -1:
+            print("error both taken")
+        elif self.low == -1 and self.high == -1:
+            self.low = val
+        elif val >= self.low:
+            self.high = val
+        elif val < self.low:
+            self.high = self.low
+            self.low = val
+        else:
+            print("error uncaught")
+        if self.low == 17 and self.high == 61:
+            print('BOT ID {} compared 61 and 17'.format(self.id))
+
+    def add_rules(self, r):
+        """add rules for the low or high chip"""
+        if 'high' in r.keys() and 'low' in r.keys():
+            self.rules = r
+        else:
+            print('bad rules')
+
+    def has_two(self):
+        """check if has two chips"""
+        if self.low != -1 and self.high != -1:
+            return True
+        else:
+            return False
+
+
+def print_bots(all_bots):
+    """print all bots and their info"""
+    print('id\tlow\thigh')
+    for k, b in all_bots.items():
+        assert k == b.id
+        print(b.id, end="\t")
+        print(b.low, end="\t")
+        print(b.high, end="\t")
+        print(b.rules['low'], end="\t")
+        print(b.rules['high'], end="\t")
+        print()
+
+        
+
+# data = open('testinput').read().splitlines()
+data = open('input').read().splitlines()
+
+bots = {}
+
+for d in data:
+    cmd = d.split()
+    if cmd[0] == 'value':
+        bot_id = int(cmd[-1])
+        chip = int(cmd[1])
+        # if bot already exists add the chip
+        if bot_id in bots:
+            bots[bot_id].add_chip(chip)
+        else:
+            bots[bot_id] = Bot(bot_id)
+            bots[bot_id].add_chip(chip)
+
+    elif cmd[0] == 'bot':
+        bot_id = int(cmd[1])
+        rules = {}
+        rules['low'] = (cmd[5], int(cmd[6]))
+        rules['high'] = (cmd[10], int(cmd[11]))
+        if bot_id in bots:
+            bots[bot_id].add_rules(rules)
+        else:
+            bots[bot_id] = Bot(bot_id)
+            bots[bot_id].add_rules(rules)
+
+# print_bots(bots)
+
+output = []
+
+while True:
+    found = False
+    for k, b in bots.items():
+        if b.has_two():
+            found = True
+            # execute low rule
+            if b.rules['low'][0] == 'output':
+                output.append(b.low)
+                b.low = -1
+            else:
+                bot_id = b.rules['low'][1]
+                if bot_id in bots:
+                    bots[bot_id].add_chip(b.low)
+                    b.low = -1
+                else:
+                    print('bot not found')
+                
+
+            # execute high rule
+            if b.rules['high'][0] == 'output':
+                output.append(b.high)
+                b.high = -1
+            else:
+                bot_id = b.rules['high'][1]
+                if bot_id in bots:
+                    bots[bot_id].add_chip(b.high)
+                    b.high = -1
+                else:
+                    print('bot not found')
+
+    if not found:
+        break
+
+# print_bots(bots)
+        
