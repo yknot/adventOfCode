92576520f5f3bb28f0fe1a2a38562749b592baa3
Andrew Yale <a.yale9@gmail.com>
2018-01-09 18:02:17 -0500

    fixed and part 2 of day 21

--- 2017/21/02.py
+++ 2017/21/02.py
@@ -5,10 +5,12 @@ start = np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1]])
 
 def transform(grid, rules):
     """transform the grid"""
+    g_sum = grid.sum()
+    g_hash = hash(bytes(grid))
     for r in rules:
-        if r.i_pat.sum() != grid.sum():
+        if r.i_sum != g_sum:
             continue
-        if np.array_equal(grid, r.i_pat):
+        if r.i_hash == g_hash:
             return r.o_pat
     raise
 
@@ -16,6 +18,8 @@ def transform(grid, rules):
 class Rule(object):
     def __init__(self, i, o):
         self.i_pat = i
+        self.i_hash = hash(bytes(i))
+        self.i_sum = i.sum()
         self.o_pat = o
 
 
@@ -26,30 +30,38 @@ def converter(tmp):
         for j, val in enumerate(line):
             if val == '#':
                 res[i, j] = 1
-    return res
+    return res.astype(int)
 
 
 def parse_rules(raw):
     """parse the raw rules"""
-    rules = []
+    rules_2 = []
+    rules_3 = []
 
     for r in raw:
         raw = r.split(' => ')
         in_pattern = converter(raw[0].split('/'))
+        two_flag = True if len(in_pattern) == 2 else False
         out_pattern = converter(raw[1].split('/'))
 
         # add all rotations of rule
         for i in range(4):
-            rules.append(Rule(in_pattern, out_pattern))
+            if two_flag:
+                rules_2.append(Rule(in_pattern, out_pattern))
+            else:
+                rules_3.append(Rule(in_pattern, out_pattern))
             in_pattern = np.rot90(in_pattern)
 
         # flip then try again
         in_pattern = np.flip(in_pattern, 1)
         for i in range(4):
-            rules.append(Rule(in_pattern, out_pattern))
+            if two_flag:
+                rules_2.append(Rule(in_pattern, out_pattern))
+            else:
+                rules_3.append(Rule(in_pattern, out_pattern))
             in_pattern = np.rot90(in_pattern)
 
-    return rules
+    return rules_2, rules_3
 
 
 def split(g):
@@ -73,10 +85,8 @@ def combine(gs):
     k = 0
     res = np.array([])
     for i in range(side_len):
-        res_sub = np.array([])
-        for j in range(side_len):
-            res_sub = gs[k] if j == 0 else np.hstack((res_sub, gs[k]))
-            k += 1
+        res_sub = np.hstack((gs[k:k + side_len]))
+        k += side_len
         res = res_sub if i == 0 else np.vstack((res, res_sub))
 
     return res
@@ -84,7 +94,7 @@ def combine(gs):
 
 def run_algo(rules_raw, num_iter):
     """run the algorithm"""
-    rules = parse_rules(rules_raw)
+    rules_2, rules_3 = parse_rules(rules_raw)
     grid = start
 
     for i in range(num_iter):
@@ -92,9 +102,10 @@ def run_algo(rules_raw, num_iter):
             print(i, int(grid.sum()), len(grid))
 
         grids = split(grid)
+        two_flag = True if len(grids[0]) == 2 else False
         new_grids = []
         for g in grids:
-            new_grids.append(transform(g, rules))
+            new_grids.append(transform(g, rules_2 if two_flag else rules_3))
 
         grid = combine(new_grids)
 
@@ -106,7 +117,7 @@ grid = run_algo(rules_raw, 2)
 assert grid.sum() == 12
 
 rules_raw = open('input').read().splitlines()
-grid = run_algo(rules_raw, 5)
+grid = run_algo(rules_raw, 18)
 print(int(grid.sum()))
 # double check
-assert grid.sum() == 164
+assert grid.sum() == 2355110
