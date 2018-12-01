1399c633f648f7d0c258b3ca4574eed28a597994
Andrew Yale <a.yale9@gmail.com>
2018-12-01 13:48:52 -0500

    2018 day 1

---
+++ 2018/01_02.py
@@ -0,0 +1,25 @@
+# read input
+nums = [int(i) for i in open('01_input').read().split('\n')]
+
+seen = set()
+tot = 0
+found = False
+
+# keep going once at the end of nums
+while 1:
+    # loop
+    for i in nums:
+        tot += i
+        # if dup freq
+        if tot in seen:
+            print(tot)
+            found = True
+            break
+            
+        # add unseen
+        seen.add(tot)
+
+    if found:
+        break
+    
+    
