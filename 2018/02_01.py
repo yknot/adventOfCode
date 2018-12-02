f198be6c27ecd4b4223019c1fa4d121471e7289c
Andrew Yale <a.yale9@gmail.com>
2018-12-02 08:59:15 -0500

    day 2

---
+++ 2018/02_01.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+
+inpt = [i for  i in open('02_input')]
+
+twos = 0
+threes = 0
+
+for line in inpt:
+    counts = defaultdict(lambda: 0)
+    for l in line:
+        counts[l] += 1
+
+    if 2 in counts.values():
+        twos += 1
+    if 3 in counts.values():
+        threes += 1
+
+print(twos * threes)
+    
