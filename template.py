995c935be2c5ade045b9894b588db7ea09caa580
Andrew Yale <a.yale9@gmail.com>
2018-12-07 08:54:43 -0500

    add a template file

---
+++ template.py
@@ -0,0 +1,24 @@
+"""Day DAYNUM"""
+import sys
+
+
+def solve_1():
+    """solver for part 1"""
+    pass
+
+
+def solve_2():
+    """solver for part 2"""
+    pass
+
+
+if __name__ == "__main__":
+    test = """ """
+
+    if len(sys.argv) == 1 or sys.argv[1] == '1':
+        assert solve_1()
+        solve_1()
+
+    elif sys.argv[1] == '2':
+        assert solve_2()
+        solve_2()
