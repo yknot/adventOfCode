2e57ca4897da7c7b37f2e6df0a8a46147e7ad388
Andrew Yale <a.yale9@gmail.com>
2022-12-08 09:09:44 -0500

    day 8

---
+++ 2022/08_01.py
@@ -0,0 +1,50 @@
+"""
+Day 8
+"""
+import numpy as np
+from utils import read_input
+
+test_inpt = """30373
+25512
+65332
+33549
+35390"""
+
+
+def count_visible(grid):
+    total = 2 * grid.shape[0] + 2 * grid.shape[1] - 4
+
+    i = 1
+    while i < (grid.shape[0] - 1):
+        j = 1
+        while j < (grid.shape[1] - 1):
+            h = grid[i, j]
+            up = grid[:i, j]
+            down = grid[i + 1 :, j]
+            left = grid[i, :j]
+            right = grid[i, j + 1 :]
+            if (
+                (h > up).all()
+                or (h > down).all()
+                or (h > left).all()
+                or (h > right).all()
+            ):
+                total += 1
+
+            j += 1
+
+        i += 1
+
+    return total
+
+
+def parser(inpt):
+    return np.array([[int(i) for i in list(line)] for line in inpt.split()])
+
+
+assert count_visible(parser(test_inpt)) == 21
+
+with open("08_input") as f:
+    inpt = parser(f.read())
+
+assert count_visible(inpt) == 1805
---
+++ 2022/08_02.py
@@ -0,0 +1,54 @@
+"""
+Day 8
+"""
+import numpy as np
+from utils import read_input
+
+test_inpt = """30373
+25512
+65332
+33549
+35390"""
+
+
+def compute_scenic(grid):
+    scenic = np.zeros(grid.shape)
+
+    i = 1
+    while i < (grid.shape[0] - 1):
+        j = 1
+        while j < (grid.shape[1] - 1):
+            h = grid[i, j]
+            up = grid[:i, j][::-1]
+            down = grid[i + 1 :, j]
+            left = grid[i, :j][::-1]
+            right = grid[i, j + 1 :]
+
+            total = 1
+            for lst in [up, down, left, right]:
+                for c, x in enumerate(lst):
+                    if x >= h:
+                        total *= c + 1
+                        break
+                else:
+                    total *= len(lst)
+
+            scenic[i, j] = total
+
+            j += 1
+
+        i += 1
+
+    return int(scenic.max())
+
+
+def parser(inpt):
+    return np.array([[int(i) for i in list(line)] for line in inpt.split()])
+
+
+assert compute_scenic(parser(test_inpt)) == 8
+
+with open("08_input") as f:
+    inpt = parser(f.read())
+
+assert compute_scenic(inpt) == 444528
