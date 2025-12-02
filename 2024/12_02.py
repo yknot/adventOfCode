63376d00272b2775015ce1bd3b2e8e5eb95e0906
Andrew Yale <a.yale9@gmail.com>
2025-12-01 20:39:09 -0700

    wip

---
+++ 2024/12_02.py
@@ -0,0 +1,127 @@
+example_1 = """AAAA
+BBCD
+BBCC
+EEEC"""
+
+example_2 = """OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO"""
+
+example_2_1 = """AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA"""
+
+example_3 = """RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE"""
+
+
+def parse_data(data):
+    return [list(line) for line in data.split("\n")]
+
+
+def expand_region(grid, i, j, val):
+    region = set([(i, j)])
+    region.update(expand(grid, region, i + 1, j, val))
+    region.update(expand(grid, region, i - 1, j, val))
+    region.update(expand(grid, region, i, j + 1, val))
+    region.update(expand(grid, region, i, j - 1, val))
+
+    return region
+
+
+def expand(grid, region, i, j, val):
+    if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
+        return region
+
+    if (i, j) in region:
+        return region
+
+    if grid[i][j] != val:
+        return region
+
+    region.add((i, j))
+
+    region.update(expand(grid, region, i + 1, j, val))
+    region.update(expand(grid, region, i - 1, j, val))
+    region.update(expand(grid, region, i, j + 1, val))
+    region.update(expand(grid, region, i, j - 1, val))
+
+    return region
+
+
+def calculate_sides(region):
+    sides = 4
+    for x, y in list(region):
+        if (x + 1, y) in region:
+            sides -= 1
+        if (x - 1, y) in region:
+            sides -= 1
+        if (x, y + 1) in region:
+            sides -= 1
+        if (x, y - 1) in region:
+            sides -= 1
+
+    return sides
+
+
+def calculate_area_sides(grid):
+    seen = set()
+
+    regions = []
+    for i, row in enumerate(grid):
+        for j, cell in enumerate(row):
+            # if already visited skip
+            if (i, j) in seen:
+                continue
+            seen.add((i, j))
+
+            # if not visited, start a new region
+            region = expand_region(grid, i, j, cell)
+            seen.update(region)
+
+            print(f"Region {cell}: {region}")
+            sides = calculate_sides(region)
+            regions.append((cell, len(region), sides))
+
+    total = 0
+    for _, area, sides in regions:
+        print(f"Region: {area} * {sides} = {area * sides}")
+        total += area * sides
+
+    return total
+
+
+def calculate_price(data):
+    grid = parse_data(data)
+    total = calculate_area_sides(grid)
+    return total
+
+
+res = calculate_price(example_1)
+assert res == 80, res
+
+res = calculate_price(example_2)
+assert res == 436, res
+
+res = calculate_price(example_2_1)
+assert res == 368, res
+
+res = calculate_price(example_3)
+assert res == 1206, res
+
+with open("12_input") as f:
+    res = calculate_price(f.read())
+    assert res == 1494342, res
